d17267f92939ae43d4d3290aea8b17a70d42f7e2
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-08 03:26:28 +0100

    complet advance task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     else:
-        if argv[2] != (add or sub or mul or div):
+        if sys.argv[2] != (add or sub or mul or div):
             print("Unknown operator. Available operators: +, -, * and /")
         else:
-            a = int(argv[1])
-            b = int(argv[3])
-            if argv[2] == "+":
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            if sys.argv[2] == "+":
                 print('{} + {} = {}'.format(a, b, add(a, b)))
-            elif argv[2] == "-":
+            elif sys.argv[2] == "-":
                 print('{} - {} = {}'.format(a, b, sub(a, b)))
-            elif argv[2] == "*":
+            elif sys.argv[2] == "*":
                 print('{} * {} = {}'.format(a, b, mul(a, b)))
-            else argv[2] == "/":
+            else sys.argv[2] == "/":
                 print('{} / {} = {}'.format(a, b, div(a, b)))
