ce096391bbf01b9b1687e014959334d55d124c16
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-12 01:01:05 +0100

    update task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,6 @@
 def print_list_integer(my_list=[]):
     for num in my_list:
+        if isinstance(num, int):
             print('{}'.format(num))
+        else:
+            print("Warning: Non-integer value found in the lisy.")
