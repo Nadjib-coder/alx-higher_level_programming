63bd8c2d1bd27c50d2c54cf34d73475dfe55de3b
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-12 20:12:23 +0100

    complet task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
+    length = len(sentencei)
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
     result_tuple = (length, first_char)
     return result_tuple
