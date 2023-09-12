b1f8a4bc1ddaf7a9013a20a696d7c348a85c461c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-12 20:32:09 +0100

    update task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     mult = []
     for i in range(len(my_list) - 1):
         if (my_list[i] % 2) == 0:
-            mult.append(true)
+            mult.append(True)
         else:
-            mult.append(false)
+            mult.append(False)
     return mult
