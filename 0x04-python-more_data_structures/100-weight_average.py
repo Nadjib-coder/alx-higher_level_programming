df824d33c40e95a04bfe11c50311bf6975d1b762
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-14 03:37:44 +0100

    complet task 12

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    number = 0
+    denn = 0
+    for tupp in my_list:
+        num += tupp[0] * tupp[1]
+        denn += tupp[1]
+    return number / denn
