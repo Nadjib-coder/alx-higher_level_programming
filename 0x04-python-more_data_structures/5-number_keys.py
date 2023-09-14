8a1a305a0e45d4ea6b48f2f06991549331b48f4e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-09-14 03:03:42 +0100

    update task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    result = 0
+    num = 0
     list_keys = list(a_dictionary.keys())
+
     for i in list_keys:
-        result += i
-    return result
+        num += 1
+
+    return (num)
