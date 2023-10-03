a277929b929351ed8574b13f4328e692a54037fa
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-03 01:24:30 +0100

    update task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -84,10 +84,10 @@ def recursive_solve(board, row, queens, solutions):
     """recursively solve an N-queens puzzle
 
     Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solutions (list): A list of lists of solutions.
+        board (list): The current working chessboard
+        row (int): The current working row
+        queens (int): The current number of placed queens
+        solutions (list): A list of lists of solutions
     Returns:
         solutions
     """
