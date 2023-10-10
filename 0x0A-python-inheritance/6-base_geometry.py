070209a9ee1617038ce434b819dd78afce1939c3
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-10 19:19:32 +0100

    complet task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/use/bin/python3
+""" a module to write a class with public instacne method"""
+
+
+class BaseGeometry:
+    """a BaseGeometry class"""
+    def area(self):
+        print("area() is nit implemented")
