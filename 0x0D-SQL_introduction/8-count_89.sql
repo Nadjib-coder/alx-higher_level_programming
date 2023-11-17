d0fc3bcb46d7d1522f244da848ca36a413ed5b7a
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-17 21:33:24 +0100

    update task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- a script that displays the number of records
 -- with id = 89 in the table first_table of the database
-SELECT COUNT(*) FROM file_table WHERE ID=89;
+SELECT COUNT(*) FROM first_table WHERE ID=89;
 
