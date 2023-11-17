ddcffab2189d97b35e41ee2668c67a73f95f4095
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-18 00:46:28 +0100

    complet task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- a script that creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT
-	name VARCHAR(256)
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10)
