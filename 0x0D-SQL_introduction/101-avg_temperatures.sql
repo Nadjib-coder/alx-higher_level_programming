ec051bd1c11882d47fc455e8c7549e88fed4a1a9
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-18 02:28:15 +0100

    update task 18 #advance

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,3 @@
 -- Import in hbtn_0c_0 database this table dump
-SELECT city, AVG(value) AS avg_temp FROM temperatures
-GROUP BY cityORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
 
