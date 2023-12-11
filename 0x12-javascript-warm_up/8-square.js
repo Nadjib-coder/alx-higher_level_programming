b06b54994ab2c3bf0bfbcea0f0d709fd83e78e66
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-11 20:59:44 +0100

    complet task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,9 +7,11 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= size; i++) {
-    for (let y = 0; y <= size; y++) {
-      console.log('X');
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let y = 0; y < size; y++) {
+      row += 'X';
     }
+    console.log(row);
   }
 }
