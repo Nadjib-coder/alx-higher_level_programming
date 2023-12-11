64b16a4bbb2fa4ce9a0543d1aebb58bbddd8ac6c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-11 20:09:54 +0100

    update task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 // a script that prints the first argument passed to it
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2] !== undefined) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
