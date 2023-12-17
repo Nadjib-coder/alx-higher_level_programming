59826feaef25fb016e848e6171b088a7e5623201
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-17 20:52:18 +0100

    complet task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,13 @@
 
 const fs = require('fs');
 
-exports.concats = function (file1Path, file2Path, destinationPath) {
+function concatFiles (file1Path, file2Path, destinationPath) {
   const file1Content = fs.readFileSync(file1Path, 'utf-8');
   const file2Content = fs.readFileSync(file2Path, 'utf-8');
   const concatenatedContent = file1Content + file2Content;
   fs.writeFileSync(destinationPath, concatenatedContent, 'utf-8');
-};
+}
+
+const [,, file1Path, file2Path, destinationPath] = process.argv;
+
+concatFiles(file1Path, file2Path, destinationPath);
