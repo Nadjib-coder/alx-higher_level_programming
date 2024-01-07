85891cff0b5907518aeb44221b39644b499d5a8a
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-01-08 00:12:42 +0100

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+Module to fetch and display information about
+the status of https://alx-intranet.hbtn.io
+"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+def fetch_status():
+    """
+    Fetches the status of https://alx-intranet.hbtn.io
+    and displays relevant information.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
 
-print("Body response:")
-print("\t- type:", type(html))
-print("\t- content:", html.decode('utf-8'))
-print("\t- utf8 content:", html.decode('utf-8'))
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data.decode('utf-8'))
+    print("\t- utf8 content:", data.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    fetch_status()
