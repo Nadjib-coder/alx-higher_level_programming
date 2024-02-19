78f8a6c905e4081a71838a0d111bd43167080ddc
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-02-19 23:56:49 +0100

    update

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+"""
+this module conatine for divides all elements of a matrix
+
+matrix_divided a function that divides all element of a matrix
+"""
+
+def matrix_divided(matrix, div):
+    """
+    a function that divides all elements of a matrix
+    """
